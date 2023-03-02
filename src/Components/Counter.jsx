2a8a70e216965181eefa81828d81e86765968b58
Lucas Werner <lwerner27@gmail.com>
2023-03-01 18:50:56 -0600

    Fixed UI for double digits, added decrementHelper so counter doesn't go below 0

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,17 +1,25 @@
 import { MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline'
 
 const Counter = ({className, devotion, setter}) => (
-    <div className="flex flex-col items-center">
-        <button className='w-full' onClick={() => setter(devotion + 1)}>
-            <PlusCircleIcon className="aspect-square text-blue-500" />
+    <div className="flex flex-col items-center w-1/6">
+        <button className='w-3/4' onClick={() => setter(devotion + 1)}>
+            <PlusCircleIcon className="text-blue-500" />
         </button>
         
-        <div className={`text-center rounded-full px-7 my-6 font-sans text-9xl text-white ${className}`}>{devotion}</div>
+        <div className={`rounded-full w-10/12 aspect-square my-7 flex items-center justify-center ${className}`}>
+            <div className='text-9xl text-white'>{devotion}</div>
+        </div>
         
-        <button className='w-full' onClick={() => setter(devotion - 1)}>
-            <MinusCircleIcon className="aspect-square text-blue-500" />
+        <button className='w-3/4' onClick={() => decrementHelper(devotion, setter)}>
+            <MinusCircleIcon className="text-blue-500" />
         </button>
     </div>
 )
+
+function decrementHelper(devotion, setter) {
+    if (devotion > 0) {
+        setter(devotion - 1)
+    }
+}
 
 export default Counter 
