8338c7b87425ae4c249679872d97fcbf29fe982b
Lucas Werner <lwerner27@gmail.com>
2023-03-01 18:51:47 -0600

    Fixed spelling of Forest, added a type prop for future use in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [plainsCount, setPlainsCount] = useState(0);
   const [islandCount, setIslandCount] = useState(0);
   const [mountainCount, setMountainCount] = useState(0);
-  const [forrestCount, setForrestCount] = useState(0);
+  const [forestCount, setForestCount] = useState(0);
   const [swampCount, setSwampCount] = useState(0);
 
   return (
@@ -14,11 +14,11 @@ function App() {
       <div className='container h-screen mx-auto flex items-center'>
         <div className="bg-white w-full flex justify-evenly">
 
-          <Counter className="bg-yellow-300" devotion={plainsCount} setter={setPlainsCount}></Counter>
-          <Counter className="bg-cyan-500" devotion={islandCount} setter={setIslandCount}></Counter>
-          <Counter className="bg-red-500" devotion={mountainCount} setter={setMountainCount}></Counter>
-          <Counter className="bg-green-500" devotion={forrestCount} setter={setForrestCount}></Counter>
-          <Counter className="bg-violet-500" devotion={swampCount} setter={setSwampCount}></Counter>
+          <Counter className="bg-yellow-300" type="plain" devotion={plainsCount} setter={setPlainsCount}></Counter>
+          <Counter className="bg-cyan-500" type="island" devotion={islandCount} setter={setIslandCount}></Counter>
+          <Counter className="bg-red-500" type="mountain" devotion={mountainCount} setter={setMountainCount}></Counter>
+          <Counter className="bg-green-500" type="forest"devotion={forestCount} setter={setForestCount}></Counter>
+          <Counter className="bg-violet-500" type="swamp" devotion={swampCount} setter={setSwampCount}></Counter>
 
         </div>
       </div>
